c65e06cbaae7062dbc886b622d852a8a7bc2cd0e
laurendv <laurendvu@outlook.com>
2024-01-20 12:16:04 +0000

    x

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -14,8 +14,9 @@ class Collection(models.Model):
 class Product(models.Model):
     #sku = models.CharField(max_length=10, primary_key=True) #doesn't create ID and makes SKU PK
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT) #protects collection from being deleted if it has products
@@ -53,7 +54,7 @@ class Order(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    zip = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=255)
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
 
     
@@ -69,12 +70,3 @@ class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
-
-class Tag(models.Model):
-    label = models.CharField(max_length=255)
-    
-class TaggedItem(models.Model):
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE) #ContentType is a special model that Django uses to track all the models in the project
-    object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey( )#GenericForeignKey is a special field that allows us to link to any model)
